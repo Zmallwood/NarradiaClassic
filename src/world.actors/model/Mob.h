9ed178f331509375528bc47fd90adb9572b43a7f
Zmallwood <zmallwood@proton.me>
2023-12-06 09:27:44 +0100

    Add mobs

--- src/world.actors/model/Mob.h
+++ src/world.actors/model/Mob.h
@@ -11,7 +11,21 @@ namespace Narradia {
             return type_;
         }
 
+        int ticks_last_move() {
+            return ticks_last_move_;
+        }
+
+        void set_ticks_last_move(int value) {
+            ticks_last_move_ = value;
+        }
+
+        float movement_speed() {
+            return movement_speed_;
+        }
+
       private:
         std::string_view type_;
+        int ticks_last_move_ = 0;
+        float movement_speed_ = 1.5f;
     };
 }
