5e506b9e43b45e985a0755b7576fbd1b534ff8ec
Zmallwood <zmallwood@proton.me>
2023-12-09 04:06:39 +0100

    Comments

--- src/core/model/Engine.cpp
+++ src/core/model/Engine.cpp
@@ -6,12 +6,12 @@
 namespace Narradia
 {
     /**
-    Seeds the randomization to get unique random generations each game start.
+     Seeds the randomization to get unique random generations each game start.
     */
     Engine::Engine() { srand(time(nullptr)); }
 
     /**
-    Update game logic for current frame.
+     Update game logic for current frame.
     */
     void Engine::UpdateGameLogic()
     {
