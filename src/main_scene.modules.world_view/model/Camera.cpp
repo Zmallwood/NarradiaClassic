95c8d2bb0fcebfffc1393c27480ebd4ed33d159e
Zmallwood <zmallwood@proton.me>
2023-12-09 04:40:56 +0100

    Refactor

--- src/main_scene.modules.world_view/model/Camera.cpp
+++ src/main_scene.modules.world_view/model/Camera.cpp
@@ -24,29 +24,29 @@ namespace Narradia
     {
         auto player = Player::Get();
         auto world = World::Get();
-        auto used_camera_distance = camera_distance_;
-        auto used_vertical_angle = vertical_angle_deg_;
-        auto player_position_no_elevation = player->position().Multiply(kTileSize);
-        auto dz_unrotated = CosDeg(used_vertical_angle) * used_camera_distance;
-        auto hypotenuse = dz_unrotated;
-        auto dx = SinDeg(horizontal_angle_deg_) * hypotenuse - 3.0f * SinDeg(horizontal_angle_deg_);
-        auto dz = CosDeg(horizontal_angle_deg_) * hypotenuse - 3.0f * CosDeg(horizontal_angle_deg_);
-        auto dy = SinDeg(used_vertical_angle) * used_camera_distance;
-        auto dx_player = -4.0f * SinDeg(horizontal_angle_deg_);
-        auto dz_player = -4.0f * CosDeg(horizontal_angle_deg_);
-        auto player_position = player->position().GetXZ();
+        float used_camera_distance = camera_distance_;
+        float used_vertical_angle = vertical_angle_deg_;
+        Point3F player_position_no_elevation = player->position().Multiply(kTileSize);
+        float dz_unrotated = CosDeg(used_vertical_angle) * used_camera_distance;
+        float hypotenuse = dz_unrotated;
+        float dx = SinDeg(horizontal_angle_deg_) * hypotenuse - 3.0f * SinDeg(horizontal_angle_deg_);
+        float dz = CosDeg(horizontal_angle_deg_) * hypotenuse - 3.0f * CosDeg(horizontal_angle_deg_);
+        float dy = SinDeg(used_vertical_angle) * used_camera_distance;
+        float dx_player = -4.0f * SinDeg(horizontal_angle_deg_);
+        float dz_player = -4.0f * CosDeg(horizontal_angle_deg_);
+        PointF player_position = player->position().GetXZ();
         auto map_area = World::Get()->curr_map_area();
         auto player_tile = map_area->GetTile(player_position.ToIntPoint());
-        auto elev_amount = kElevAmount;
-        auto tile_coord = player_position.ToIntPoint();
+        float elev_amount = kElevAmount;
+        Point tile_coord = player_position.ToIntPoint();
         auto tile = map_area->GetTile(tile_coord);
-        auto elev00 = static_cast<float>(tile->elevation());
-        auto elev10 = elev00;
-        auto elev11 = elev00;
-        auto elev01 = elev00;
-        auto coord10 = tile_coord.Translate(1, 0);
-        auto coord11 = tile_coord.Translate(1, 1);
-        auto coord01 = tile_coord.Translate(0, 1);
+        float elev00 = static_cast<float>(tile->elevation());
+        float elev10 = elev00;
+        float elev11 = elev00;
+        float elev01 = elev00;
+        Point coord10 = tile_coord.Translate(1, 0);
+        Point coord11 = tile_coord.Translate(1, 1);
+        Point coord01 = tile_coord.Translate(0, 1);
 
         if (map_area->IsInsideMap(coord10))
             elev10 = map_area->GetTile(coord10)->elevation();
@@ -57,12 +57,12 @@ namespace Narradia
         if (map_area->IsInsideMap(coord01))
             elev01 = map_area->GetTile(coord01)->elevation();
 
-        auto tile_avg_elev = (elev00 + elev10 + elev01 + elev11) / 4.0f;
-        auto player_tile_dx = player_position.x - static_cast<int>(player_position.x) - 0.5f;
-        auto player_tile_dy = player_position.y - static_cast<int>(player_position.y) - 0.5f;
-        auto elev_dx = ((elev10 - elev00) + (elev11 - elev01)) / 2.0f;
-        auto elev_dy = ((elev01 - elev00) + (elev11 - elev10)) / 2.0f;
-        auto player_elev = tile_avg_elev + player_tile_dx * elev_dx + player_tile_dy * elev_dy;
+        float tile_avg_elev = (elev00 + elev10 + elev01 + elev11) / 4.0f;
+        float player_tile_dx = player_position.x - static_cast<int>(player_position.x) - 0.5f;
+        float player_tile_dy = player_position.y - static_cast<int>(player_position.y) - 0.5f;
+        float elev_dx = ((elev10 - elev00) + (elev11 - elev01)) / 2.0f;
+        float elev_dy = ((elev01 - elev00) + (elev11 - elev10)) / 2.0f;
+        float player_elev = tile_avg_elev + player_tile_dx * elev_dx + player_tile_dy * elev_dy;
 
         return player_position_no_elevation.Translate(dx, dy + player_elev * elev_amount, dz);
     }
