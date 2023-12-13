c04a6dd4a1233fbe0b9ba129f0d746bb48f597ff
Zmallwood <zmallwood@proton.me>
2023-12-13 20:44:47 +0100

    Small adjustments

--- client/src/main_scene.modules.world_view/model/camera.h
+++ client/src/main_scene.modules.world_view/model/camera.h
@@ -25,8 +25,8 @@ namespace Narradia
 
         float camera_height_ = 3.0f;
         float used_fov_ = 90.0f;
-        float camera_distance_ = 10.0f;
+        float camera_distance_ = 20.0f;
         float horizontal_angle_deg_ = 0.0f;
-        float vertical_angle_deg_ = 130.0f;
+        float vertical_angle_deg_ = 160.0f;
     };
 }
--- map_generator/src/world.generation/model/command/generate_elevation.cpp
+++ map_generator/src/world.generation/model/command/generate_elevation.cpp
@@ -19,31 +19,13 @@ namespace Narradia
                if (dx * dx + dy * dy <= r * r) {
                   if (map_area->GetTile(x, y)->ground() != "GroundWater")
                      map_area->GetTile(x, y)->IncreaseElevation(
-                         0.4 * static_cast<float>(r_max) /
+                         0.2 * static_cast<float>(r_max) /
                          (r + 50.0f * kMapWidth / 200.0f * kMapHeight / 160.0f));
                }
             }
          }
       }
-      auto num_spikes = static_cast<int>(20 * (kMapWidth / 100.0f * kMapHeight / 80.0f));
-      for (auto i = 0; i < num_spikes; i++) {
-         auto x_center = rand() % kMapWidth;
-         auto y_center = rand() % kMapHeight;
-         auto r = 3 + rand() % 7;
-         for (auto y = y_center - r; y <= y_center + r; y++) {
-            for (auto x = x_center - r; x <= x_center + r; x++) {
-               if (x < 0 || y < 0 || x >= kMapWidth || y >= kMapHeight)
-                  continue;
-               auto dx = x - x_center;
-               auto dy = y - y_center;
-               if (dx * dx + dy * dy <= r * r) {
-                  if (map_area->GetTile(x, y)->ground() != "GroundWater")
-                     map_area->GetTile(x, y)->IncreaseElevation(0.1f);
-               }
-            }
-         }
-      }
-      auto num_hills = static_cast<int>(20 * (kMapWidth / 100.0f * kMapHeight / 80.0f));
+      auto num_hills = static_cast<int>(10 * (kMapWidth / 100.0f * kMapHeight / 80.0f));
       for (auto i = 0; i < num_hills; i++) {
          auto x_center = rand() % kMapWidth;
          auto y_center = rand() % kMapHeight;
